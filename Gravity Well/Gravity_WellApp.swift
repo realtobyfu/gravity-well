561728bd717fc1308f59f7fe2aa819695cb8bdd7
realtobyfu <zfu04@tufts.edu>
2025-09-15 13:25:42 +0200

    Initial Commit

---
+++ Gravity Well/Gravity_WellApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gravity_WellApp.swift
+//  Gravity Well
+//
+//  Created by Tobias Fu on 15/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct Gravity_WellApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
